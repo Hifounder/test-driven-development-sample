a0ec9be1eb99093736ced9831b6ba27b4e0d9807
Hifounder <hifounder0719@gmail.com>
2022-11-21 20:15:04 +0800

    feat(all): addition feature for Moneys in the same currency done

--- go/money_test.go
+++ go/money_test.go
@@ -15,6 +15,21 @@ func (m Money) Divide(divisor int) Money {
 	return Money{amount: m.amount / float64(divisor), currency: m.currency}
 }
 
+type Portfolio []Money
+
+func (p Portfolio) Add(m Money) Portfolio {
+	p = append(p, m)
+	return p
+}
+
+func (p Portfolio) Evaluate(currency string) Money {
+	total := 0.0
+	for _, m := range p {
+		total = total + m.amount
+	}
+	return Money{amount: total, currency: currency}
+}
+
 func assertEqual(t *testing.T, expected Money, actual Money) {
 	if expected != actual {
 		t.Errorf("Expected [%+v] Got [%+v]", expected, actual)
@@ -35,4 +50,18 @@ func TestDivision(t *testing.T) {
 	actual := originalMoney.Divide(4)
 	expected := Money{amount: 1000.5, currency: "KRW"}
 	assertEqual(t, expected, actual)
+}
+func TestAddition(t *testing.T) {
+	var portfolio Portfolio
+	var portfolioInDollars Money
+
+	fiveDollars := Money{amount: 5, currency: "USD"}
+	tenDollars := Money{amount: 10, currency: "USD"}
+	fifteenDollars := Money{amount: 15, currency: "USD"}
+
+	portfolio = portfolio.Add(fiveDollars)
+	portfolio = portfolio.Add(tenDollars)
+	portfolioInDollars = portfolio.Evaluate("USD")
+
+	assertEqual(t, fifteenDollars, portfolioInDollars)
 }
--- py/test_money.py
+++ py/test_money.py
@@ -1,3 +1,5 @@
+import functools
+import operator
 import unittest
 
 class Money:
@@ -13,6 +15,17 @@ class Money:
 
   def times(self, multiplier):
     return Money(self.amount * multiplier, self.currency)
+
+class Portfolio:
+    def __init__(self):
+        self.moneys = []
+
+    def add(self, *moneys):
+        self.moneys.extend(moneys)
+
+    def evaluate(self, currency):
+        total = functools.reduce(operator.add, map(lambda m: m.amount, self.moneys), 0)
+        return Money(total, currency)
 
 class TestMoney(unittest.TestCase):
   def testMultiplicationInDollars(self):
@@ -36,5 +49,13 @@ class TestMoney(unittest.TestCase):
     expectedMoneyAfterDivision = Money(1000.5, "KRW")
     self.assertEqual(expectedMoneyAfterDivision, originalMoney.divide(4))
 
+  def testAddition(self):
+    fiveDollars = Money(5, "USD")
+    tenDollars = Money(10, "USD")
+    fifteenDollars = Money(15, "USD")
+    portfolio = Portfolio()
+    portfolio.add(fiveDollars, tenDollars)
+    self.assertEqual(fifteenDollars, portfolio.evaluate("USD"))
+    
 if __name__ == '__main__':
     unittest.main()
