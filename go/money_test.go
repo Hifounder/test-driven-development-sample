dbe9f7daa604b45edf7a96635d25af3f8b5603da
Hifounder <hifounder0719@gmail.com>
2022-11-21 19:23:31 +0800

    feat(all): division and multiplication features done.

--- go/money_test.go
+++ go/money_test.go
@@ -2,20 +2,37 @@ package main
 
 import "testing"
 
-type Dollar struct {
-	amount int
+type Money struct {
+	amount   float64
+	currency string
 }
 
-func (d Dollar) Times(multiplier int) Dollar {
-	return Dollar{amount: d.amount * multiplier}
+func (m Money) Times(multiplier int) Money {
+	return Money{amount: m.amount * float64(multiplier), currency: m.currency}
 }
 
-func Testmultiplication(t *testing.T) {
-	fiver := Dollar{
-		amount: 5,
+func (m Money) Divide(divisor int) Money {
+	return Money{amount: m.amount / float64(divisor), currency: m.currency}
+}
+
+func assertEqual(t *testing.T, expected Money, actual Money) {
+	if expected != actual {
+		t.Errorf("Expected [%+v] Got [%+v]", expected, actual)
 	}
-	tenner := fiver.Times(2)
-	if tenner.amount != 10 {
-		t.Errorf("Expected 10, got:[%d]", tenner.amount)
+}
+
+func TestMultiplicationInDollars(t *testing.T) {
+	fiver := Money{
+		amount:   5,
+		currency: "USD",
 	}
+	actual := fiver.Times(2)
+	expected := fiver.Times(2)
+	assertEqual(t, expected, actual)
+}
+func TestDivision(t *testing.T) {
+	originalMoney := Money{amount: 4002, currency: "KRW"}
+	actual := originalMoney.Divide(4)
+	expected := Money{amount: 1000.5, currency: "KRW"}
+	assertEqual(t, expected, actual)
 }
--- js/test_money.js
+++ js/test_money.js
@@ -1,13 +1,24 @@
 const assert = require("assert");
 
-class Dollar {
-  constructor(amount){
-    this.amount = amount
+class Money {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
   }
-  times(multiplier){
-    return new Dollar(this.amount * multiplier)
+  times(multiplier) {
+    return new Money(this.amount * multiplier, this.currency);
+  }
+
+  divide(divisor) {
+    return new Money(this.amount / divisor, this.currency);
   }
 }
-let fiver = new Dollar(5)
-let tenner = fiver.times(2)
-assert.strictEqual(tenner.amount, 10)
+
+let fiveDollars = new Money(5, "USD");
+let tenDollars = new Money(10, "USD");
+assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
+
+let originalMoney = new Money(4002, "KRW");
+let actualResult = originalMoney.divide(4);
+let expectResult = new Money(1000.5, "KRW");
+assert.deepStrictEqual(actualResult, expectResult);
