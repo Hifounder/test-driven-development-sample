5980c154a0eb489cecc8024eeb68c88e51a4d824
Hifounder <hifounder0719@gmail.com>
2022-11-22 12:59:30 +0800

    refactor(go): move Money and Portfolio to stocks Go pkgs

--- go/money_test.go
+++ go/money_test.go
@@ -1,63 +1,35 @@
 package main
 
-import "testing"
+import (
+	s "tdd/stocks"
+	"testing"
+)
 
-type Money struct {
-	amount   float64
-	currency string
-}
-
-func (m Money) Times(multiplier int) Money {
-	return Money{amount: m.amount * float64(multiplier), currency: m.currency}
-}
-
-func (m Money) Divide(divisor int) Money {
-	return Money{amount: m.amount / float64(divisor), currency: m.currency}
-}
-
-type Portfolio []Money
-
-func (p Portfolio) Add(m Money) Portfolio {
-	p = append(p, m)
-	return p
-}
-
-func (p Portfolio) Evaluate(currency string) Money {
-	total := 0.0
-	for _, m := range p {
-		total = total + m.amount
-	}
-	return Money{amount: total, currency: currency}
-}
-
-func assertEqual(t *testing.T, expected Money, actual Money) {
+func assertEqual(t *testing.T, expected s.Money, actual s.Money) {
 	if expected != actual {
 		t.Errorf("Expected [%+v] Got [%+v]", expected, actual)
 	}
 }
 
 func TestMultiplicationInDollars(t *testing.T) {
-	fiver := Money{
-		amount:   5,
-		currency: "USD",
-	}
+	fiver := s.NewMoney(5, "USD")
 	actual := fiver.Times(2)
 	expected := fiver.Times(2)
 	assertEqual(t, expected, actual)
 }
 func TestDivision(t *testing.T) {
-	originalMoney := Money{amount: 4002, currency: "KRW"}
+	originalMoney := s.NewMoney(4002, "KRW")
 	actual := originalMoney.Divide(4)
-	expected := Money{amount: 1000.5, currency: "KRW"}
+	expected := s.NewMoney(1000.5, "KRW")
 	assertEqual(t, expected, actual)
 }
 func TestAddition(t *testing.T) {
-	var portfolio Portfolio
-	var portfolioInDollars Money
+	var portfolio s.Portfolio
+	var portfolioInDollars s.Money
 
-	fiveDollars := Money{amount: 5, currency: "USD"}
-	tenDollars := Money{amount: 10, currency: "USD"}
-	fifteenDollars := Money{amount: 15, currency: "USD"}
+	fiveDollars := s.NewMoney(5, "USD")
+	tenDollars := s.NewMoney(10, "USD")
+	fifteenDollars := s.NewMoney(15, "USD")
 
 	portfolio = portfolio.Add(fiveDollars)
 	portfolio = portfolio.Add(tenDollars)
